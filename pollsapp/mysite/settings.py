2507ad8063e06e767660ff57ece1be50cedb3728
Robert <bobm123@gmail.com>
2015-07-15 06:31:07 -0400

    create polls app and connect to PostgresDB

--- pollsapp/mysite/settings.py
+++ pollsapp/mysite/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'polls',
 )
 
 MIDDLEWARE_CLASSES = (
@@ -75,9 +76,17 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # https://docs.djangoproject.com/en/1.8/ref/settings/#databases
 
 DATABASES = {
+    #'default': {
+    #    'ENGINE': 'django.db.backends.sqlite3',
+    #    'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    #}
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'PollsDB',
+        'USER': 'robert',
+        'PASSWORD': 'mitleid123',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
