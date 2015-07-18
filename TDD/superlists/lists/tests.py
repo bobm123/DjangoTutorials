b4aff7e98318e4abf3e1b69c7c681205df29aaf8
Robert <bobm123@gmail.com>
2015-07-18 09:55:52 -0400

    had one item added to a list working, added a test that forces app to work with multiple items

--- TDD/superlists/lists/tests.py
+++ TDD/superlists/lists/tests.py
@@ -2,6 +2,9 @@ from django.core.urlresolvers import resolve
 from django.test import TestCase
 from django.http import HttpRequest
 
+from django.shortcuts import render
+from django.template.loader import render_to_string
+
 from lists.views import home_page
 
 
@@ -29,3 +32,18 @@ class HomePageTest(TestCase):
         self.assertTrue(response.content.startswith(b'<html>'))
         self.assertIn(b'<title>To-Do lists</title>', response.content)
         self.assertTrue(response.content.endswith(b'</html>'))
+        
+    def test_home_page_can_save_a_POST_request(self):
+        myListItem = 'A new list item'
+        request = HttpRequest()
+        request.method = 'POST'
+        request.POST['item_text'] = myListItem
+    
+        response = home_page(request)
+    
+        self.assertIn(myListItem, response.content.decode())
+        expected_html = render_to_string(
+            'home.html',
+            {'new_item_text':  myListItem}
+        )
+        self.assertEqual(response.content.decode(), expected_html)
