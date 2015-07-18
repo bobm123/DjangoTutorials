92dde1bfc1cd373a549616dfdbfb558df884aebb
Robert <bobm123@gmail.com>
2015-07-18 06:42:46 -0400

    Refactored code to move html into template. Added some debug prints() to tests when that didn;t work at first.

--- TDD/superlists/lists/tests.py
+++ TDD/superlists/lists/tests.py
@@ -23,6 +23,10 @@ class HomePageTest(TestCase):
     def test_home_page_returns_correct_html(self):
         request = HttpRequest()
         response = home_page(request)
+        print ('***************')
+        print (response.content)
+        print ('***************')
+        print (type(response.content).__name__)
         self.assertTrue(response.content.startswith(b'<html>'))
         self.assertIn(b'<title>To-Do lists</title>', response.content)
-        self.assertTrue(response.content.endswith(b'</html>'))
+        self.assertTrue(response.content.strip().endswith(b'</html>'))
