4a4f732c87cc3b2af0e7ea6caf3bcbc3143fa728
Robert <bobm123@gmail.com>
2015-07-18 10:05:14 -0400

    refactored the code for checking table values

--- TDD/functional_tests.py
+++ TDD/functional_tests.py
@@ -6,6 +6,8 @@ import time
 import unittest
 
 
+
+
 class NewVisitorTest(unittest.TestCase):
     
     def setUp(self):
@@ -14,7 +16,13 @@ class NewVisitorTest(unittest.TestCase):
         
     def tearDown(self):
         self.browser.quit()
-        
+
+    # A method to help with checking table contents
+    def check_for_row_in_list_table(self, row_text):
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn(row_text, [row.text for row in rows])
+
     def test_can_start_a_list_and_retrieve_it_later(self):
         # Edith has heard about a cool new online to-do app. She goes
         # to check out its homepage
@@ -51,6 +59,7 @@ class NewVisitorTest(unittest.TestCase):
         #)
         # a better way to write that assertion (list comprehension FTW!)
         #self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
+        self.check_for_row_in_list_table('1: Buy peacock feathers')
         
         # There is still a text box inviting her to add another item. She
         # enters "Use peacock feathers to make a fly" (Edith is very methodical)
@@ -64,10 +73,9 @@ class NewVisitorTest(unittest.TestCase):
         rows = table.find_elements_by_tag_name('tr')
         
         # make sure it still has first entry plus the new one
-        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
-        self.assertIn(
-            '2: Use peacock feathers to make a fly',
-            [row.text for row in rows]
+        self.check_for_row_in_list_table('1: Buy peacock feathers')
+        self.check_for_row_in_list_table(
+            '2: Use peacock feathers to make a fly'
         )
 
         # Edith wonders whether the site will remember her list. Then she sees
