71a4207c425616f060ed5b60d1780b2e8a997b59
Robert <bobm123@gmail.com>
2015-07-18 14:42:21 -0400

    Had to tweek it a bit so it woudl run under python 2.7

--- TDD/superlists/functional_tests/tests.py
+++ TDD/superlists/functional_tests/tests.py
@@ -2,9 +2,9 @@ from django.test import LiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
-import time
+#import time
 
-import unittest
+#import unittest
 
 class NewVisitorTest(LiveServerTestCase):
 
@@ -42,21 +42,12 @@ class NewVisitorTest(LiveServerTestCase):
         # is tying fly-fishing lures)
         inputbox.send_keys('Buy peacock feathers')
 
-        # When she hits enter, the page updates, and now the page lists
-        # "1: Buy peacock feathers" as an item in a to-do list
+         # When she hits enter, she is taken to a new URL,
+        # and now the page lists "1: Buy peacock feathers" as an item in a
+        # to-do list table
         inputbox.send_keys(Keys.ENTER)
-
-        #time.sleep(10) # wait a moment before shutting down browser
-        #table = self.browser.find_element_by_id('id_list_table')
-        #rows = table.find_elements_by_tag_name('tr')
-        #self.assertTrue(
-        #    any(row.text == '1: Buy peacock feathers' for row in rows),
-        #    "New to-do item did not appear in table - text was:\n%s" % (
-        #        table.text
-        #    )
-        #)
-        # a better way to write that assertion (list comprehension FTW!)
-        #self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
+        edith_list_url = self.browser.current_url
+        self.assertRegexpMatches(edith_list_url, '/lists/.+') ## assertRegex in py 3
         self.check_for_row_in_list_table('1: Buy peacock feathers')
 
         # There is still a text box inviting her to add another item. She
@@ -76,6 +67,37 @@ class NewVisitorTest(LiveServerTestCase):
             '2: Use peacock feathers to make a fly'
         )
 
+        # Now a new user, Francis, comes along to the site.
+    
+        ## We use a new browser session to make sure that no information
+        ## of Edith's is coming through from cookies etc #1
+        self.browser.quit()
+        self.browser = webdriver.Firefox()
+    
+        # Francis visits the home page. There is no sign of Edith's list
+        self.browser.get(self.live_server_url)
+        page_text = self.browser.find_element_by_tag_name('body').text
+        self.assertNotIn('Buy peacock feathers', page_text)
+        self.assertNotIn('make a fly', page_text)
+    
+        # Francis starts a new list by entering a new item. He
+        # is less interesting than Edith...
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Buy milk')
+        inputbox.send_keys(Keys.ENTER)
+    
+        # Francis gets his own unique URL
+        francis_list_url = self.browser.current_url
+        self.assertRegexpMatches(francis_list_url, '/lists/.+') 
+        self.assertNotEqual(francis_list_url, edith_list_url)
+    
+        # Again, there is no trace of Edith's list
+        page_text = self.browser.find_element_by_tag_name('body').text
+        self.assertNotIn('Buy peacock feathers', page_text)
+        self.assertIn('Buy milk', page_text)
+    
+        # Satisfied, they both go back to sleep
+        
         # Edith wonders whether the site will remember her list. Then she sees
         # that the site has generated a unique URL for her -- there is some
         # explanatory text to that effect.
@@ -84,8 +106,7 @@ class NewVisitorTest(LiveServerTestCase):
 
         # Satisfied, she goes back to sleep
 
-        self.fail('Finish the test!')
 
 
-if __name__ == '__main__':
-    unittest.main()
+#if __name__ == '__main__':
+#    unittest.main()
