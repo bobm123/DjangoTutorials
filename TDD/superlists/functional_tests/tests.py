f65785105d8824a328915f24b08f49f2d144bf13
Robert <bobm123@gmail.com>
2015-07-18 20:46:37 -0400

    Use Bootstrap to improve layout

--- TDD/superlists/functional_tests/tests.py
+++ TDD/superlists/functional_tests/tests.py
@@ -1,4 +1,4 @@
-from django.test import LiveServerTestCase
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
@@ -6,7 +6,7 @@ from selenium.webdriver.common.keys import Keys
 
 #import unittest
 
-class NewVisitorTest(LiveServerTestCase):
+class NewVisitorTest(StaticLiveServerTestCase):
 
     def setUp(self):
         self.browser = webdriver.Firefox()
@@ -35,7 +35,7 @@ class NewVisitorTest(LiveServerTestCase):
         self.assertAlmostEqual(
             inputbox.location['x'] + inputbox.size['width'] / 2,
             512,
-            delta=5
+            delta=10
         )
     
     # A method to help with checking table contents
