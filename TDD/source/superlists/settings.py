f27f651c2ef751c476e189391e1d6b12610bbf8e
Robert <bobm123@gmail.com>
2015-07-26 19:33:43 -0400

    turns off debug and sets ALLOWED_HOSTS

--- TDD/source/superlists/settings.py
+++ TDD/source/superlists/settings.py
@@ -23,9 +23,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'vw@oyxhd$qx!y5^*ar&*l*m2#9%q186oz4s*i+frqtaj!xck^b'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+#DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
