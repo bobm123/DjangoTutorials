da676cc87958a2e14187410844f66a0be705ee8f
Robert <bobm123@gmail.com>
2015-07-27 08:39:03 -0400

    some tweeks for fabfile

--- TDD/source/superlists/settings.py
+++ TDD/source/superlists/settings.py
@@ -23,10 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'vw@oyxhd$qx!y5^*ar&*l*m2#9%q186oz4s*i+frqtaj!xck^b'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-#DEBUG = True
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = []
 
 
 # Application definition
