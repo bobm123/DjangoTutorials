a7a95aebe040858253aa8fbbf9058589f9497045
Robert <bobm123@gmail.com>
2015-08-03 08:40:35 -0400

    Custom Persona auth backend + custom user model

--- TDD/source/superlists/settings.py
+++ TDD/source/superlists/settings.py
@@ -84,6 +84,12 @@ DATABASES = {
 }
 
 
+AUTH_USER_MODEL = 'accounts.User'
+AUTHENTICATION_BACKENDS = (
+    'accounts.authentication.PersonaAuthenticationBackend',
+)
+
+
 # Internationalization
 # https://docs.djangoproject.com/en/1.8/topics/i18n/
 
--- TDD/source/superlists/urls.py
+++ TDD/source/superlists/urls.py
@@ -15,11 +15,13 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
-from lists import views as list_views  #1
-from lists import urls as list_urls  #2
+from lists import views as list_views
+from lists import urls as list_urls
+from accounts import urls as account_urls
 
 urlpatterns = [
     url(r'^$', list_views.home_page, name='home'),
+    url(r'^accounts/', include(account_urls)),
     url(r'^lists/', include(list_urls)),
     #url(r'^admin/', include(admin.site.urls)),
 ]
